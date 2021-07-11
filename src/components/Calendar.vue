ec0dc83329c8cbd429e4803d783cc7d7e676d011
takumi555 <kato.takumi1998@gmail.com>
2021-07-11 19:35:45 +0900

    コンポーネント間でデータを共有

---
+++ src/components/Calendar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Calendar</h1>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ events }}
+      </li>
+    </ul>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
+
+    <CalendarDetails />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import CalendarDetails from './CalendarDetails.vue';
+
+export default {
+  name: 'Calendar',
+  components: {
+    CalendarDetails,
+  },
+  computed: {
+    ...mapGetters('events', ['events']),
+  },
+  methods: {
+    ...mapActions('events', ['fetchEvents'])
+  },
+};
+</script>
