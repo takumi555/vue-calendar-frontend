24b90b6664c2db0f5ac5f510b39ba7aa1d12cf0d
takumi555 <kato.takumi1998@gmail.com>
2021-07-15 23:47:10 +0900

    開始日と終了日選択フォームの実装

--- src/components/EventFormDialog.vue
+++ src/components/EventFormDialog.vue
@@ -9,6 +9,10 @@
       <DialogSection icon="mdi-square" :color="event.color || 'blue'">
         <v-text-field v-model="name" label="タイトル"></v-text-field>
       </DialogSection>
+      <DialogSection icon="mdi-clock-oiitline">
+        <DateForm v-model="startDate" />
+        <DateForm v-model="endDate" />
+      </DialogSection>
     </v-card-text>
     <v-card-actions class="d-flex justify-end">
       <v-btn @click="submit">保存</v-btn>
@@ -19,17 +23,26 @@
 <script>
 import { mapGetters, mapActions } from 'vuex';
 import DialogSection from './DialogSection';
+import DateForm from './DateForm';
+import { format } from 'date-fns';
 
 export default {
   name: 'EventFormDialog',
   components: {
     DialogSection,
+    DateForm,
   },
   data: () => ({
     name: '',
+    startDate: null,
+    endDate: null,
   }),
   computed: {
     ...mapGetters('events', ['event']),
+  },
+  created() {
+    this.startDate = format(this.event.start, 'yyyy/MM/dd');
+    this.endDate = format(this.event.end, 'yyyy/MM/dd');
   },
   methods: {
     ...mapActions('events', ['setEvent', 'setEditMode', 'createEvent']),
@@ -40,8 +53,8 @@ export default {
     submit() {
       const params = {
         name: this.name,
-        start: this.event.start,
-        end: this.event.end,
+        start: this.startDate,
+        end: this.endDate,
       };
       this.createEvent(params);
       this.closeDialog();
